a69ef72c948bb2d8bb96a87890f4560476729722
Luca Cotti <lucacotti@outlook.com>
2021-11-22 11:20:18 +0100

    Completa esercizio scorsa lezione

--- 2021_11_16/es_composita.m
+++ 2021_11_16/es_composita.m
@@ -4,25 +4,33 @@ xa=-5;xb=5;
 x1=linspace(xa,xb,1000)';
 yf=f(x1);
 
-n=100;
-x=linspace(xa,xb,n+1);
-y=f(x);
-% Interpolatore composito lineare
-yp1c=interp1(x,y,x1);
+ERR1=[];ERR2=[];H=[];
+for n=10:10:500
+    H=[H,(xb-xa)/n];
+    x=linspace(xa,xb,n+1);
+    y=f(x);
+    % Interpolatore composito lineare
+    yp1c=interp1(x,y,x1);
+    
+    %spline
+    ys=spline(x,y,x1);
+    
+    errp1c=norm(yf-yp1c,inf);
+    ERR1=[ERR1;errp1c];
+    errs=norm(yf-ys,inf);
+    ERR2=[ERR2;errs];
 
-%spline
-ys=spline(x,y,x1);
+    fprintf("n=%d, errp1c=%e, errs=%e\n",n,errp1c,errs);
+end
 
-errp1c=norm(yf-yp1c,inf);
-errs=norm(yf-ys,inf);
-fprintf("n=%d, errp1c=%e, errs=%e\n",n,errp1c,errs);
-
-figure(1); clf
-plot(x1,yf,"LineWidth",2,"DisplayName","f(x)")
-legend("-dynamicLegend")
+%% Grafico errori
+figure(2); clf
+loglog(H,ERR1,"LineWidth",2,"DisplayName","Errore interp comp lin")
 hold on
-plot(x1,yp1c,"LineWidth",2,"DisplayName","p1c")
-plot(x1,ys,"LineWidth",2,"DisplayName","spline")
 grid on
-xlabel("x");ylabel("y")
-legend("Location","northwest")
+legend("-DynamicLegend")
+legend("Location","southeast")
+loglog(H,ERR2,"LineWidth",2,"DisplayName","Errore spline")
+loglog(H,H.^2,"DisplayName","H^2")
+loglog(H,H.^4,"DisplayName","H^4")
+xlabel("H"); ylabel("ERR")
